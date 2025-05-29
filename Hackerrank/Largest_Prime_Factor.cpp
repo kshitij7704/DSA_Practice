10e46b2a2396237d47cb7c3c941d65f364f94454
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-05-29 09:59:18 +0530

    Create Largest_Prime_Factor.cpp

---
+++ Hackerrank/Largest_Prime_Factor.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+bool isPrime(long long n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (long long i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    }
+    return true;
+}
+
+int main()
+{
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+    long n = stol(ltrim(rtrim(n_temp)));
+    long long maxPrime = -1;
+
+    while (n % 2 == 0) {
+        maxPrime = 2;
+        n /= 2;
+    }
+
+    for (long long i = 3; i * i <= n; i += 2) {
+        while (n % i == 0) {
+            maxPrime = i;
+            n /= i;
+        }
+    }
+
+    if (n > 2)
+        maxPrime = n;
+    cout << maxPrime << endl;
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
