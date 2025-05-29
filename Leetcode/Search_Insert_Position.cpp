63f395f6b088e6c67ab881e4dd1824a82e35379f
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-05-29 22:25:03 +0530

    Create Search_Insert_Position.cpp

---
+++ Leetcode/Search_Insert_Position.cpp
@@ -0,0 +1,15 @@
+// leetcode - 35
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int ind;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] >= target){
+                ind =  i;
+                break;
+            }
+        }
+        return ind;
+    }
+};
