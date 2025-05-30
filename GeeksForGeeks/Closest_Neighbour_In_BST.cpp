bc95ab2f0bfa41d594a54e7007f552e0d1fdfc97
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-05-30 10:19:21 +0530

    Create Closest_Neighbour_In_BST.cpp

---
+++ GeeksForGeeks/Closest_Neighbour_In_BST.cpp
@@ -0,0 +1,35 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+    int findMaxFork(Node* root, int k) {
+        // code here
+        int res = -1;
+        while(root != NULL){
+            if(root->data == k){
+                res = root->data;
+                break;
+            }
+            if(root->data < k){
+                res = root->data;
+                root = root->right;
+            }
+            else{
+                root = root->left;
+            }
+        }
+        return res;
+    }
+};
