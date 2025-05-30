72fd841e187010fbf13317ef51e0b0188c05e640
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-05-30 15:44:24 +0530

    Update Single_Operation_Part_1.cpp

--- CodeChef/Single_Operation_Part_1.cpp
+++ CodeChef/Single_Operation_Part_1.cpp
@@ -1,32 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long getVal(const string &s) {
-    long long x = 0;
-    for (char c : s) {
-        x = x * 2 + (c - '0');
-    }
-    return x;
-}
-
 int main() {
     int t;
-    cin >> t;
-    while (t--) {
+    cin>>t;
+    while(t--){
         int n;
+        cin>>n;
         string s;
-        cin >> n >> s;
-        long long x = getVal(s);
-        int bestY = 1;
-        long long maxX = x ^ (x >> 1);
-        for(int y = 2; y <= n; y++){
-            long long temp = x ^ (x >> y);
-            if(temp > maxX){
-                maxX = temp;
-                bestY = y;
-            }
+        cin>>s;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        
+            if(s[i]=='1')
+            {
+            sum++;
         }
-        cout << bestY << endl;
+        else 
+        {
+        break;
+        }
+        cout<<sum<<endl;
     }
-    return 0;
+	return 0;
 }
