8eb044a6eb877b134fc2a3712a9bcbb428725e75
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-01 23:16:02 +0530

    Create Distribute_Candies_Among_Children_2.cpp

---
+++ Leetcode/Distribute_Candies_Among_Children_2.cpp
@@ -0,0 +1,15 @@
+// leetcode - 2929
+
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        long long res = 0;
+        for(int i=0;i<=min(n, limit);i++){
+            if(n - i > 2 * limit){
+                continue;
+            }
+            res += min(n - i, limit) - max(0, n - i - limit) + 1;
+        }
+        return res;
+    }
+};
