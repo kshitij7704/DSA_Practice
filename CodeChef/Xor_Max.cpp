25d88f71bf40c63a44e2a127785e1ef84fb95e36
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-09 23:51:54 +0530

    Update Xor_Max.cpp

--- CodeChef/Xor_Max.cpp
+++ CodeChef/Xor_Max.cpp
@@ -1,7 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main() {
 	// your code goes here
-
+	int tc; cin>>tc;
+	while(tc--){
+	    string a,b;
+	    cin>>a;
+	    cin>>b;
+	    int cnt1=0, n = a.size();
+	    for(int i=0; i<n; i++){
+	        if(a[i] == '1') cnt1++;
+	        if(b[i] == '1') cnt1++;
+	    }
+	    //cout<<"cnt1= "<<cnt1<<"the size is: "<<n<<endl;
+	    int mini = min(cnt1,n*2-cnt1);
+	    //cout<<"mini is: "<<mini<<endl;
+	    for(int i=0;i<mini;i++)
+	        cout<<1;
+	    for(int i=0;i<n-mini;i++)
+	        cout<<0;
+	    cout<<endl;
+	}
+	return 0;
 }
