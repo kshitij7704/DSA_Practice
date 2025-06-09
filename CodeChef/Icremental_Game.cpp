ab7bfc580a11a0325edff6587bc5aeed620a2d15
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-09 23:43:38 +0530

    Create Icremental_Game.cpp

---
+++ CodeChef/Icremental_Game.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y, k;
+        cin >> x >> y >> k;
+        if ((x - k) <= k && y <= k || (y - k) <= k && x <= k) {
+            cout << "Alice\n";
+        }
+        else {
+            cout << "Bob\n";
+        }
+    }
+}
