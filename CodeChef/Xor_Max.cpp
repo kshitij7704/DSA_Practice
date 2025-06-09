0bc5b5188a3159e75b99835fe5387541449d5edc
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-09 23:53:09 +0530

    Update Xor_Max.cpp

--- CodeChef/Xor_Max.cpp
+++ CodeChef/Xor_Max.cpp
@@ -11,14 +11,18 @@ int main() {
 	    cin>>b;
 	    int cnt1=0, n = a.size();
 	    for(int i=0; i<n; i++){
-	        if(a[i] == '1') cnt1++;
-	        if(b[i] == '1') cnt1++;
+	        if(a[i] == '1') 
+	            cnt1++;
+	        if(b[i] == '1') 
+	            cnt1++;
 	    }
 	    int mini = min(cnt1,n*2-cnt1);
-	    for(int i=0;i<mini;i++)
+	    for(int i=0;i<mini;i++){
 	        cout<<1;
-	    for(int i=0;i<n-mini;i++)
+	    }
+	    for(int i=0;i<n-mini;i++){
 	        cout<<0;
+	    }
 	    cout<<endl;
 	}
 	return 0;
