59067650e612808ebd4d824f413642d70ff27a60
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-09 23:55:01 +0530

    Update Sum_Of_Palindromic_Number.cpp

--- CodeChef/Sum_Of_Palindromic_Number.cpp
+++ CodeChef/Sum_Of_Palindromic_Number.cpp
@@ -1,7 +1,35 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
+bool isPalindrome(int n){
+    int rev = 0;
+    int a = n;
+    while(n){
+        rev = rev*10 + n%10;
+        n=n/10;
+    }
+    if(rev==a){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 int main() {
 	// your code goes here
-
+	int t;
+	cin>>t;
+	while(t--){
+	    int L, R;
+	    cin>>L>>R;
+	    int sum = 0;
+	    for(int i=L;i<=R;i++){
+	        if(isPalindrome(i)){
+	            sum = sum + i;
+	        }
+	    }
+	    cout<<sum<<endl;
+	}
+	return 0;
 }
