8df7876e3f54305f6165b16d4dfefa0d16984510
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-09 23:47:02 +0530

    Update Chef_And_Mean.cpp

--- CodeChef/Chef_And_Mean.cpp
+++ CodeChef/Chef_And_Mean.cpp
@@ -1,7 +1,56 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include<vector>
+#include<limits>
+#include<map>
+#include<cmath>
+#include<algorithm>
 using namespace std;
-
-int main() {
-	// your code goes here
-
+#define ll long long int
+#define for(i,p) for(int i=0;i<p;i++)
+//int main() {
+ void  code(int t)
+   {
+    ll p;
+    cin>>p;
+    vector<ll>n(p);
+    for(i,p)
+    {
+        cin>>n[i];
+    }
+    double me=0;
+    bool ct=true;
+    ll ans;
+    for(i,p)
+    {
+        me+=n[i];
+    }
+    
+    me/=(p*1.0);
+    for(i,p)
+    {
+        if(me==n[i])
+        {
+            ct=false;
+            ans=i+1;
+            break;
+        }
+    }
+    if(ct)
+    {
+        cout<<"Impossible\n";
+    }
+    else 
+    cout<<ans<<"\n";
+    
+   }
+   int main()
+   {
+       int t;
+       cin>>t;
+       while(t--)
+       {
+           code(t);
+       }
+   
+	return 0;
 }
