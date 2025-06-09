df0fbb3a9ce76316e54131e946740c65c3e85d1c
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-09 23:45:12 +0530

    Update Days_In_Month.cpp

--- CodeChef/Days_In_Month.cpp
+++ CodeChef/Days_In_Month.cpp
@@ -1,6 +1,34 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-	// your code goes here
+    int t;
+    cin >> t;
+	while(t--)
+	{
+	    int n, i, d; cin >> n;
+	    int a[7]={4,4,4,4,4,4,4};
+	    string day; cin >> day;
+	    
+	    if(day=="mon") d=0;
+	    else if(day=="tues") d=1;
+	    else if(day=="wed") d=2;
+	    else if(day=="thurs") d=3;
+	    else if(day=="fri") d=4;
+	    else if(day=="sat") d=5;
+	    else if(day=="sun") d=6;
+	    
+	    for(i=29; i<=n; i++)
+	    {
+	        a[d]++;
+	        if(d==6) d=-1;
+	        d++;
+	    }
+	    
+	    for(i=0; i<7; i++)
+	      cout << a[i] << " ";
+	    cout << endl;
+	}
+	return 0;
 }
