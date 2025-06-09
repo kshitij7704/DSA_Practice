8104d5a98e8ab320b49c0e218bb428d0357e2f92
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-09 11:04:59 +0530

    Create Kth_Smallest_Lexicographical_Order.cpp

---
+++ Leetcode/Kth_Smallest_Lexicographical_Order.cpp
@@ -0,0 +1,34 @@
+// leetcode - 440
+
+
+// TLE
+class Solution {
+private:
+    void find(int i, vector<int> &res, int n){
+        //Base case
+        if(i > n){
+            return;
+        }
+        //Adding the number first
+        res.push_back(i);
+        for(int j=0;j<10;j++){
+            //Calling recursive method for each child of the number
+            find(i * 10 + j, res, n);
+        }
+    }
+
+    vector<int> getLexicalOrder(int n) {
+        vector<int> res;
+        for(int i=1;i<=9;i++){
+            //Calling recursive method for each digit from 1-9
+            find(i, res, n);
+        }
+        return res;
+    }
+
+public:
+    int findKthNumber(int n, int k) {
+        vector<int> lexical = getLexicalOrder(n);
+        return lexical[k - 1];
+    }
+};
