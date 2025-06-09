e7a9fc60c560f5e1bd54f2f2fa9a114e4e65e064
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-09 11:09:45 +0530

    Update Kth_Smallest_Lexicographical_Order.cpp

--- Leetcode/Kth_Smallest_Lexicographical_Order.cpp
+++ Leetcode/Kth_Smallest_Lexicographical_Order.cpp
@@ -32,3 +32,39 @@ public:
         return lexical[k - 1];
     }
 };
+
+
+// Accepted
+class Solution {
+private:
+    using int64 = long long;
+    int64 countPrefix(int64 prefix, int n) {
+        int64 count = 0;
+        int64 curr = prefix;
+        int64 next = prefix + 1;
+        while(curr <= n){
+            count += min((int64)n + 1, next) - curr;
+            curr *= 10;
+            next *= 10;
+        }
+        return count;
+    }
+
+public:
+    int findKthNumber(int n, int k) {
+        int64 curr = 1;
+        k -= 1;
+        while(k > 0){
+            int64 count = countPrefix(curr, n);
+            if(count <= k){
+                curr++;
+                k -= count;
+            } 
+            else{
+                curr *= 10;
+                k -= 1;
+            }
+        }
+        return curr;
+    }
+};
