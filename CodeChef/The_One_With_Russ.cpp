2131a8436e19cb551163cf29d2293d75c2634ba2
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-10 23:41:50 +0530

    Update The_One_With_Russ.cpp

--- CodeChef/The_One_With_Russ.cpp
+++ CodeChef/The_One_With_Russ.cpp
@@ -3,5 +3,24 @@ using namespace std;
 
 int main() {
 	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x,k;
+	    cin>>n>>x>>k;
+	    vector<int>a(n),b(n);
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    for(int i=0;i<n;i++){
+	        cin>>b[i];
+	    }
+	    int count=0;
+	    for(int i=0;i<n;i++){
+	        if(abs(a[i]-b[i])<=k) count++;
+	    }
+	    if(count>=x) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
 
 }
