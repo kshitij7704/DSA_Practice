7ff71916d441be76e48daa8ff199a477fd404859
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-10 23:43:15 +0530

    Update The_One_With_Russ.cpp

--- CodeChef/The_One_With_Russ.cpp
+++ CodeChef/The_One_With_Russ.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
 	// your code goes here
 	int t;
-	cin>>t;
+	cin >> t;
 	while(t--){
 	    int n,x,k;
-	    cin>>n>>x>>k;
+	    cin >> n >> x >> k;
 	    vector<int>a(n),b(n);
 	    for(int i=0;i<n;i++){
 	        cin>>a[i];
@@ -22,11 +22,11 @@ int main() {
 	        }
 	    }
 	    if(count>=x){
-	        cout<<"YES"<<endl;
+	        cout << "YES" << endl;
 	    }
 	    else{
-	        cout<<"NO"<<endl;
+	        cout << "NO" << endl;
 	    }
 	}
-
+	return 0;
 }
