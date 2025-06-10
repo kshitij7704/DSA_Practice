1d2146eb58647f08cf83158f1a7255e5ef5646a0
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-10 14:19:08 +0530

    Update Exactly_One_Swap.cpp

--- GeeksForGeeks/Exactly_One_Swap.cpp
+++ GeeksForGeeks/Exactly_One_Swap.cpp
@@ -2,11 +2,13 @@ class Solution {
   public:
     int countStrings(string &s) {
         // code here
-        unordered_set<string> st;
         int n = s.size();
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (i == j) continue;
+        unordered_set<string> st;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(i == j){
+                    continue;
+                }
                 swap(s[i], s[j]);
                 st.insert(s);
                 swap(s[i], s[j]);
