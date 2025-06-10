cc22931d841c037c744e565b089bdb35d0b0632b
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-10 14:17:38 +0530

    Update Exactly_One_Swap.cpp

--- GeeksForGeeks/Exactly_One_Swap.cpp
+++ GeeksForGeeks/Exactly_One_Swap.cpp
@@ -1,7 +1,20 @@
 class Solution {
   public:
     int countStrings(string &s) {
-        // code here
+        unordered_set<string> uniqueStrings;
+        int n = s.size();
         
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (i == j) continue;
+                
+                // Swap characters
+                swap(s[i], s[j]);
+                uniqueStrings.insert(s);
+                // Swap back to original
+                swap(s[i], s[j]);
+            }
+        }
+        return uniqueStrings.size();
     }
 };
