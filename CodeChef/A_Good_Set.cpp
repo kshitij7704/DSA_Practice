c3226fc621cfedb1f21ff00f6888cf3489c607be
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-10 23:50:35 +0530

    Update A_Good_Set.cpp

--- CodeChef/A_Good_Set.cpp
+++ CodeChef/A_Good_Set.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main() {
 	// your code goes here
 	int t;
-	cin>>t;
-	for(int i=0;i<t;i++){
+	cin >> t;
+	while(t--){
 	    int n;
-	    cin>>n;
+	    cin >> n;
 	    for(int i=1;i<2*n;i+=2){
-	        cout<<i<<" ";
-	    } cout<<endl;
+	        cout << i << " ";
+	    } 
+	    cout << endl;
 	}
 	return 0;
 }
