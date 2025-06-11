9b9fbc04c71977c5f38af9d045aca84c70d1ce44
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-11 19:25:21 +0530

    Update Weapon_Choice.cpp

--- CodeChef/Weapon_Choice.cpp
+++ CodeChef/Weapon_Choice.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main() {
-	// your code goes here
-
+	int t;
+	cin>>t;
+	while(t--){
+	    long long h,x,y1,y2,k;
+	    long long a,b;
+	    cin>>h>>x>>y1>>y2>>k;
+	    a=ceil(h/(1.0*x));
+	    long long temp=h-(k*y1);
+	    if(temp<=0){b=ceil(h/(1.0*y1));}
+	    else{b=ceil(temp/(1.0*y2))+k;}
+	    if(a>b){cout<<b;}
+	    else{cout<<a;}
+	    cout<<endl;
+	}
 }
