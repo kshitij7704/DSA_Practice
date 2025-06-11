e206b320f19f404ce9704f315145d0b14ddc5df6
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-11 19:18:40 +0530

    Update Remove_The_Balls.cpp

--- GeeksForGeeks/Remove_The_Balls.cpp
+++ GeeksForGeeks/Remove_The_Balls.cpp
@@ -2,6 +2,19 @@ class Solution {
   public:
     int findLength(vector<int> &color, vector<int> &radius) {
         // code here
-        
+        stack<pair<int, int>> stk;
+
+        for (int i = 0; i < color.size(); ++i) {
+            pair<int, int> current = {color[i], radius[i]};
+            
+            if (!stk.empty() && stk.top() == current) {
+                // Remove both if top of stack is the same as current
+                stk.pop();
+            } else {
+                stk.push(current);
+            }
+        }
+
+        return stk.size();
     }
 };
