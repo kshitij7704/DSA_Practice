d9d8bca6c6811af84234e0b524d2d3591a02f8a8
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-12 10:51:03 +0530

    Update Grid_Xor.cpp

--- CodeChef/Grid_Xor.cpp
+++ CodeChef/Grid_Xor.cpp
@@ -1,7 +1,17 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 int main() {
-	// your code goes here
-
+    int t;
+    cin >> t;
+    while(t--){
+        int k;
+        cin >> k;
+        for(int i=0;i<k;i++){
+            for(int i=0;i<k;i++){
+                cout << "1 ";
+            }
+            cout << endl;
+        }  
+    }
+    return 0;
 }
