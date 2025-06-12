23b10604266c233fb8e1eb204686ba61e568b515
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-12 23:38:17 +0530

    Update Beautiful_Garden.cpp

--- CodeChef/Beautiful_Garden.cpp
+++ CodeChef/Beautiful_Garden.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 
 int main() {
-	// your code goes here
-
+    int t;
+    for (cin >> t; t--; ) {
+        int n, k, d;
+        cin >> n >> k >> d;              
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            a[i] = 1 + (d - 1) / x;        
+        }
+    
+        sort(a.rbegin(), a.rend());
+        
+        long long ans = 0;                
+        int z = max(0, n - k);            
+        for (int i = 0; i < z; i++) {
+            ans += a[i];
+        }
+        
+        cout << ans << endl;
+    }
+    
 }
