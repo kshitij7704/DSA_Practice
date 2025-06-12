ba179304b46d576715733f0e7b0878b6d53826f6
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-12 10:43:51 +0530

    Update Maximum_Difference_Between_Adjacent_Elements_In_Circular_Array.cpp

--- Leetcode/Maximum_Difference_Between_Adjacent_Elements_In_Circular_Array.cpp
+++ Leetcode/Maximum_Difference_Between_Adjacent_Elements_In_Circular_Array.cpp
@@ -1,1 +1,14 @@
 // leetcode - 3423
+
+class Solution {
+public:
+    int maxAdjacentDistance(vector<int>& nums) {
+        int res = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            int diff = abs(nums[i % n] - nums[(i+1) % n]);
+            res = max(res, diff);
+        }
+        return res;
+    }
+};
