8b119ea02c07f873767ee30c648fb1aacdb82bfd
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-12 10:49:05 +0530

    Update K_Closest_Elements.cpp

--- GeeksForGeeks/K_Closest_Elements.cpp
+++ GeeksForGeeks/K_Closest_Elements.cpp
@@ -2,6 +2,27 @@ class Solution {
   public:
     vector<int> printKClosest(vector<int> arr, int k, int x) {
         // Code here
-        
+        priority_queue<pair<int, int>> maxHeap;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i] == x){
+                continue;
+            }
+            int dist = abs(arr[i] - x);
+            maxHeap.push({dist, -arr[i]});
+            if(maxHeap.size() > k){
+                maxHeap.pop();
+            }
+        }
+        vector<int> result;
+        while (!maxHeap.empty()) {
+            result.push_back(-maxHeap.top().second);
+            maxHeap.pop();
+        }
+        sort(result.begin(), result.end(), [&](int a, int b){
+            int d1 = abs(a - x), d2 = abs(b - x);
+            if (d1 != d2) return d1 < d2;
+            return a > b;
+        });
+        return result;
     }
 };
