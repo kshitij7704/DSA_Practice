bd397dc36cd646e62855fdf0b1cd5b9d8ead9073
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-12 23:36:57 +0530

    Update Bigger_Is_Better.cpp

--- CodeChef/Bigger_Is_Better.cpp
+++ CodeChef/Bigger_Is_Better.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 int main() {
 	// your code goes here
-	int t; cin >> t;
+	int t; 
+	cin >> t;
 	while(t--) {
-	    int n; cin >> n; string s; cin >> s;
-	    
+	    int n; 
+	    cin >> n; 
+	    string s; 
+	    cin >> s;
 	    bool b = 0;
-	    for(int i = 0; i < n; i++) if(s[i] != 'z') b = 1;
+	    for(int i = 0; i < n; i++) 
+	        if(s[i] != 'z') 
+	            b = 1;
 	    if(!b) {
 	        cout << -1 << "\n";
 	    }
-	    
 	    else {
 	        for(int i = 0; i < n; i++) {
-	        cout << 'z';
-	    }
-	    cout << "\n";
+	            cout << 'z';
+	        }
+	        cout << "\n";
 	    }
 	}
 	return 0;
