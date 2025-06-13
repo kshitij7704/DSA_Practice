6fe56cc34c320e8b43679f2404f51ab5c7a8208f
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-13 23:39:13 +0530

    Update Ceaser_Cipher.cpp

--- CodeChef/Ceaser_Cipher.cpp
+++ CodeChef/Ceaser_Cipher.cpp
@@ -1,7 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s,t,u;
+        cin>>s>>t>>u;
+        int m=t[0]-s[0];
+        if (m < 0)
+            m += 26;
+        string v;
+        for(int i=0;i<n;i++){
+            char c = (u[i] - 'a' + m) % 26 + 'a';
+            v += c;
+        }
+        cout<<v<<endl;
+    }
+    
+	return 0;
 }
