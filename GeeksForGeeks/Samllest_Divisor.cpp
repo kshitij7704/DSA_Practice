d1f90596f9d91b8f58b91c6163c97e2e88a9a7c0
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-15 01:02:31 +0530

    Update Samllest_Divisor.cpp

--- GeeksForGeeks/Samllest_Divisor.cpp
+++ GeeksForGeeks/Samllest_Divisor.cpp
@@ -2,6 +2,23 @@ class Solution {
   public:
     int smallestDivisor(vector<int>& arr, int k) {
         // Code here
-        
+        int low = 1;
+        int high = *max_element(arr.begin(), arr.end());
+        int res = high;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            int sum = 0;
+            for(int num : arr){
+                sum += (num + mid - 1) / mid;
+            }
+            if(sum <= k){
+                res = mid;
+                high = mid - 1;
+            } 
+            else{
+                low = mid + 1;
+            }
+        }
+        return res;   
     }
 };
