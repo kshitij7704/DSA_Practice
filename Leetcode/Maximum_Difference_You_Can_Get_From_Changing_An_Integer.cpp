f1925b2da01201dde3598c63fe0834cb99aa5258
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-15 10:51:58 +0530

    Update Maximum_Difference_You_Can_Get_From_Changing_An_Integer.cpp

--- Leetcode/Maximum_Difference_You_Can_Get_From_Changing_An_Integer.cpp
+++ Leetcode/Maximum_Difference_You_Can_Get_From_Changing_An_Integer.cpp
@@ -1,8 +1,31 @@
 // leetcode - 1432
 
 class Solution {
+private:
+    string change(int num, int x, int y){
+        string numStr = to_string(num);
+        for(char i : numStr){
+            if(i - '0' == x){
+                i = '0' + y;
+            }
+        }
+        return numStr;
+    }
+
 public:
     int maxDiff(int num) {
-        
+        int mini = num;
+        int maxi = num;
+        for(int x=0;x<10;x++){
+            for(int y=0;y<10;y++){
+                string res = change(num, x, y);
+                if(res[0] != '0'){
+                    int temp = stoi(res);
+                    mini = min(mini, temp);
+                    maxi = max(maxi, temp);
+                }
+            }
+        }
+        return maxi - mini;
     }
 };
