db4cb9dc940bc67f50085978883211bc4c707112
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-16 22:55:13 +0530

    Update Maximum_Difference_Between_Increasing_Elements.cpp

--- Leetcode/Maximum_Difference_Between_Increasing_Elements.cpp
+++ Leetcode/Maximum_Difference_Between_Increasing_Elements.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
-        
+        int n = nums.size();
+        int res = INT_MIN;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                res = max(res, nums[j] - nums[i]);
+            }
+        }
+        return res;
     }
 };
