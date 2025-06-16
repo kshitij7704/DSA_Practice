80976358c4aa66e8eaea699d1c9476936dba4377
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-16 23:00:27 +0530

    Update Distinct_Codes.cpp

--- CodeChef/Distinct_Codes.cpp
+++ CodeChef/Distinct_Codes.cpp
@@ -1,7 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-
+    int t;
+    cin >> t;
+    while (t--){
+        string s;
+        cin >> s;
+        int e = s.length();
+        set < string > s2;
+        for (int i = 0; i < e - 1; i++){
+            string s1 = s.substr(i, 2);
+            s2.insert(s1);
+        }
+        cout << s2.size() << endl;
+    }
+    return 0;
 }
