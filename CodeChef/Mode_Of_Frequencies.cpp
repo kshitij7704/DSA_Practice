0e99bf209f0b85829e53218c2da1e88661d1dda9
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-16 23:03:56 +0530

    Update Mode_Of_Frequencies.cpp

--- CodeChef/Mode_Of_Frequencies.cpp
+++ CodeChef/Mode_Of_Frequencies.cpp
@@ -2,6 +2,38 @@
 using namespace std;
 
 int main() {
-	// your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        unordered_map<int, int> m1;
 
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            m1[x]++;
+        }
+        unordered_map<int, int> m2;
+        
+        for (auto i : m1) {
+            m2[i.second]++;
+        }
+        int maxi = 0;
+        int ans=0;
+        for (auto i : m2) {
+            if(i.second>maxi)
+            {
+                ans=i.first;
+                maxi=i.second;
+            }
+            else if(i.second==maxi)
+            {
+                ans=min(ans,i.first);
+            }
+            
+        }
+        cout << ans << endl;
+    }
+    return 0;
 }
