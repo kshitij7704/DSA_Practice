1efb2cb95033cd2c7fffea41b7568fbfe94734cf
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-16 22:51:29 +0530

    Update Equilize_The_Towers.cpp

--- GeeksForGeeks/Equilize_The_Towers.cpp
+++ GeeksForGeeks/Equilize_The_Towers.cpp
@@ -1,7 +1,32 @@
 class Solution {
+  using ll = long long;
+  private:
+     ll calculateCost(vector<int>& heights,vector<int>& cost, int target){
+        ll Tcost = 0;
+        for(int i=0;i<heights.size();i++){
+            Tcost += 1LL * abs(heights[i] - target) * cost[i];
+        }
+        return Tcost;
+     }
+  
   public:
     int minCost(vector<int>& heights, vector<int>& cost) {
         // code here
-        
+        int low = *min_element(heights.begin(), heights.end());
+        int high = *max_element(heights.begin(), heights.end());
+        ll res = LLONG_MAX;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            ll cMid = calculateCost(heights, cost, mid);
+            ll cMidPlusOne = calculateCost(heights, cost, mid + 1);
+            res = min(res, min(cMid, cMidPlusOne));
+            if(cMid < cMidPlusOne){
+                high = mid - 1;
+            } 
+            else{
+                low = mid + 1;
+            }
+        }
+        return res;  
     }
 };
