ac7c6ae12ac2b4c11da0ee217790a2b9958cd116
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-16 22:57:59 +0530

    Update Maximum_Difference_Between_Increasing_Elements.cpp

--- Leetcode/Maximum_Difference_Between_Increasing_Elements.cpp
+++ Leetcode/Maximum_Difference_Between_Increasing_Elements.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
         int n = nums.size();
-        int res = INT_MIN;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                res = max(res, nums[j] - nums[i]);
+        int res = -1;
+        int preMin = nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i] > preMin){
+                res = max(res, nums[i] - preMin);
+            }
+            else{
+                preMin = nums[i];
             }
         }
         return res;
