fa1e3ce8b59612fe4c65b57d219325aa35de9002
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-18 22:08:58 +0530

    Update Disabled_King.cpp

--- CodeChef/Disabled_King.cpp
+++ CodeChef/Disabled_King.cpp
@@ -1,7 +1,15 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
 
 int main() {
 	// your code goes here
-
+    int t=0;
+	cin>>t;
+	while (t--) {
+        int n=0;
+	    cin>>n;
+	    cout<<((n % 2 == 0) ? n : (n - 1))<<endl;
+	}
+	return 0;
 }
