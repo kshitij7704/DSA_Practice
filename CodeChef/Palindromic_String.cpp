81c9b1cdf0a8dd7d04abc1489cba622898a4d1d9
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-18 22:12:19 +0530

    Update Palindromic_String.cpp

--- CodeChef/Palindromic_String.cpp
+++ CodeChef/Palindromic_String.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
-	// your code goes here
-
+	int t;
+	cin >> t;
+	while(t--){
+	    string a,b;
+	    cin >> a >> b;
+	    int p = 0;
+	    for (int i=0;i<a.length();i++){
+	        if (b.find(a[i])!=-1){
+	            p=1;
+	            break;
+	        }
+	    }
+	    if (p==1) cout << "Yes" << endl;
+	    else cout << "No" << endl;
+	}
+	return 0;
 }
