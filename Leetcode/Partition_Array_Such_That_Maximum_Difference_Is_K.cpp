b2a32244a08974782a2f23c7d26c987676cf43bb
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-19 11:22:26 +0530

    Update Partition_Array_Such_That_Maximum_Difference_Is_K.cpp

--- Leetcode/Partition_Array_Such_That_Maximum_Difference_Is_K.cpp
+++ Leetcode/Partition_Array_Such_That_Maximum_Difference_Is_K.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int partitionArray(vector<int>& nums, int k) {
-        
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int res = 1;
+        int temp = nums[0];
+        for(int i=0;i<n;i++){
+            if(nums[i] - temp > k){
+                res++;
+                temp = nums[i];
+            }
+        }
+        return res;
     }
 };
