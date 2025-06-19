4b7eafcd2ab5f057910d6503b5b63be7ae1f451a
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-19 19:24:13 +0530

    Update Case_Specific_Sorting_Of_Strings.cpp

--- GeeksForGeeks/Case_Specific_Sorting_Of_Strings.cpp
+++ GeeksForGeeks/Case_Specific_Sorting_Of_Strings.cpp
@@ -2,6 +2,33 @@ class Solution {
   public:
     string caseSort(string& s) {
         // code here
-        
+        int n = s.length();
+        vector<bool> pos(n);
+        vector<char> up;
+        vector<char> low;
+        for(int i=0;i<n;i++){
+            if(isupper(s[i])){
+                pos[i] = true;
+                up.push_back(s[i]);
+            }
+            else{
+                pos[i] = false;
+                low.push_back(s[i]);
+            }
+        }
+        sort(up.begin(), up.end());
+        sort(low.begin(), low.end());
+        int l = 0;
+        int u = 0;
+        string res = "";
+        for(int i=0;i<pos.size();i++){
+            if(pos[i]){
+                res += up[u++];
+            }
+            else{
+                res += low[l++];
+            }
+        }
+        return res;
     }
 };
