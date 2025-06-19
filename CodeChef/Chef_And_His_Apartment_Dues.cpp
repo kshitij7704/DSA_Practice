c98cda9ed9a0eb0f514382fc941c66b8774ba8b0
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-19 19:37:58 +0530

    Update Chef_And_His_Apartment_Dues.cpp

--- CodeChef/Chef_And_His_Apartment_Dues.cpp
+++ CodeChef/Chef_And_His_Apartment_Dues.cpp
@@ -1,7 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-	// your code goes here
-
+    int t;
+    cin >> t;
+    
+    for(int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        int s[n];
+        int fine = 0;
+        int count = 0;
+        
+        for(int j = 0; j < n; j++) {
+            cin >> s[j];
+            
+            if(s[j] == 0) {
+                count++;
+                fine += 100;
+            }
+            else if(count>0){
+                fine+=100;
+            }
+        }
+        
+        int dues = count * 1000 + fine;
+        cout << dues << endl;
+    }
+    
+    return 0;
 }
