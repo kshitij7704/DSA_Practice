e4d8d2beab90bf46862eb55a7712f699bf577fae
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-20 21:05:17 +0530

    Update Swapping_Chefs_Way.cpp

--- CodeChef/Swapping_Chefs_Way.cpp
+++ CodeChef/Swapping_Chefs_Way.cpp
@@ -2,38 +2,38 @@
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;
-	cin>>t;
-	while(t--){
-	    int n;
-	    cin>>n;
-	    string s,a;
-	    cin>>s;
-	    a =s;
-	    sort(a.begin() ,a.end());
-	 
-	    if(a ==s){
-	        cout<<"yes"<<endl;
-	    }
-	    else{
-	        int i=0;
-	        int j = n-1;
-	        while(i<j){
-	            if(s[i]>s[j]){
-	                swap(s[i] , s[j]);
-	            }
-	            i++;
-	            j--;
-	        }
-	        
-	        if(a==s){
-	            cout<<"yes"<<endl;
-	        }
-	        else{
-	            cout<<"no"<<endl;
-	        }
-	    }
-	}
-	return 0;
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s, a;
+        cin >> s;
+        a = s;
+        sort(a.begin(), a.end());
+
+        if (a == s) {
+            cout << "yes" << endl;
+        }
+        else {
+            int i = 0;
+            int j = n - 1;
+            while (i < j) {
+                if (s[i] > s[j]) {
+                    swap(s[i], s[j]);
+                }
+                i++;
+                j--;
+            }
+
+            if (a == s) {
+                cout << "yes" << endl;
+            }
+            else {
+                cout << "no" << endl;
+            }
+        }
+    }
+    return 0;
 }
