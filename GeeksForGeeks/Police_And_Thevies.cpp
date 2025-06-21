5a0e8af2b258c4f1b7c0fe8dda2d64e4da8642d0
Kshitij Kashyap <146460546+kshitij7704@users.noreply.github.com>
2025-06-21 23:07:02 +0530

    Update Police_And_Thevies.cpp

--- GeeksForGeeks/Police_And_Thevies.cpp
+++ GeeksForGeeks/Police_And_Thevies.cpp
@@ -2,6 +2,30 @@ class Solution {
   public:
     int catchThieves(vector<char> &arr, int k) {
         // Code here
-        
+        vector<int> policemen;
+        vector<int> thieves;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i] == 'P'){
+                policemen.push_back(i);
+            }
+            else if(arr[i] == 'T'){
+                thieves.push_back(i);
+            }
+        }
+        int i = 0, j = 0, caught = 0;
+        while(i < policemen.size() && j < thieves.size()){
+            if(abs(policemen[i] - thieves[j]) <= k){
+                caught++;
+                i++;
+                j++;
+            } 
+            else if(policemen[i] < thieves[j]){
+                i++;
+            } 
+            else{
+                j++;
+            }
+        }
+        return caught;
     }
 };
